d04ed7ecd226d49abc69c76b80fc7956cb796510
mi81ma <mi81ma@gmail.com>
2018-12-16 14:34:34 +0800

    Hotdog or Not Hotdog on Real Device

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
 
         imagePicker.delegate = self
-        imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing = false
 
     }
@@ -32,6 +32,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
              imageView.image = userPickedImage
+
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+
+            detect(image: ciimage)
         }
 
 
@@ -42,12 +48,44 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 
 
+    func detect(image: CIImage) {
+
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed.")
+        }
+
+        let request = VNCoreMLRequest(model: model) { (request, error) in guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+        }
+
+        let handler = VNImageRequestHandler(ciImage: image)
+
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+    }
 
 
 
     @IBAction func cameraTapped(_ sender: Any) {
 
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+
         present(imagePicker, animated: true, completion: nil)
+
     }
 
 }
